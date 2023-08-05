9e1f351a052d106ec4f60b0a8e0ad8e51344bc31
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-08-04 19:59:18 -0500

    Card Deal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ styles }) => {
 	return (
 		<button
 			type='button'
-			className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient outline-none ${styles}`}
+			className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient outline-none ${styles} rounded-[20px]`}
 		>
 			Get Started
 		</button>
